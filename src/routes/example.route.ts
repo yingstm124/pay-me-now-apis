7ed712391cfb142b9c2998005740f9eaf75a1b4d
yingstm <yingstm124@gmail.com>
2022-01-04 00:15:53 +0700

    test heroku is work

--- src/routes/example.route.ts
+++ src/routes/example.route.ts
@@ -3,6 +3,6 @@ import * as exampleController from "../controllers/example.controller";
 
 const router = express.Router({ mergeParams: true });
 
-router.route("/").get(exampleController.get);
+router.route("/test/").get(exampleController.get);
 
 export default router;
