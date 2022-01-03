b25b7adcc5830aff3c364bb7e1ab24ec5ad73e5e
yingstm <yingstm124@gmail.com>
2022-01-04 01:02:34 +0700

    update ts, jest config , package json

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,21 @@
 import type { Config } from "@jest/types";
+
+
 // Sync object
 const config: Config.InitialOptions = {
   verbose: true,
   transform: {
     "^.+\\.ts?$": "ts-jest",
   },
-  // globals: {
-  //   "ts-jest": {
-  //     tsconfig: "tsconfig.json",
-  //   },
-  // },
-  // moduleFileExtensions: ["ts", "js"],
-  // testMatch: ["**/test/**/*.test.(ts|js)"],
-  // testEnvironment: "node",
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+    },
+  },
+  moduleFileExtensions: ["ts", "tsx","js"],
+  moduleDirectories: ["node_modules","src"],
+  testMatch: ["**/test/**/*.test.(ts|js)"],
+  testEnvironment: "node",
+  preset: "ts-jest"
 };
 export default config;
